c4a8d102ffe77ab3ac33be4f9b4e28f209eef1ef
Hector Pulido <87735459+hepulido@users.noreply.github.com>
2023-11-20 16:40:50 -0500

    Feature: Created get resquest for band endpoint and added unit test too (#3)

--- index.test.js
+++ index.test.js
@@ -5,7 +5,7 @@ execSync('npm run seed');
 
 const request = require("supertest")
 const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
+const { Musician, Band } = require('./models/index')
 const app = require('./src/app');
 const seedMusician = require("./seedData");
 
@@ -23,6 +23,24 @@ describe('/musicians endpoint', () => {
     
     it("returns all musicians", async () => {
         const response = await request(app).get("/musicians")
+        const responseData = JSON.parse(response.text)
+        expect(responseData.length).toBe(3)
+    })
+    
+})
+describe('/Bands endpoint', () => {
+    it("returns all bands", async () => {
+        const response = await request(app).get("/bands")
+        expect(response.body.length).toBe(3)
+    })
+   
+    it("returns 200 status", async () => {
+        const response = await request(app).get("/bands")
+        expect(response.statusCode).toBe(200)
+    })
+    
+    it("returns all bands", async () => {
+        const response = await request(app).get("/bands")
         const responseData = JSON.parse(response.text)
         expect(responseData.length).toBe(3)
     })
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
@@ -8,6 +8,15 @@ const port = 3000;
 app.get('/musicians',async(req, res) => {
  const data = await Musician.findAll();
  res.json(data);
+})
+app.get('/musicians/1',async(req, res) => {
+    const data = await Musician.findOne();
+    res.json(data);
+   })
+
+app.get('/bands', async (req,res) => {
+const data = await Band.findAll();
+res.json(data);
 })
 
 
